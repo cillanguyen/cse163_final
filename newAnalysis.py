0e071a0912f79b748b48cebc7609b817d52611a1
cillanguyen <cillangu@uw.edu>
2022-06-01 00:30:13 -0700

    line + graph done + analysis

--- newAnalysis.py
+++ newAnalysis.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import geopandas as gpd
 import seaborn as sns
-# from scipy.interpolate import make_interp_spline
 import matplotlib.pyplot as plt
 
 
@@ -17,46 +16,56 @@ def main():
     df['NUM'] = count
     df = df[['CATEGORY', 'PRIMARYDISTRICTCD', 'NUM', 'UNITDESC',
              'SECONDARYDISTRICTCD', 'SHAPE_Length']]
-
-    # use groupby to keep track of the number of bike infrastructures
     year = df.groupby([df.index.year, 'CATEGORY']).sum()
-    year = year.loc[2010.0: 2020.0]
-    sns.lineplot(data=year, x='INSTALL_DATE', y='NUM', hue='CATEGORY')
+    year = year.loc[2010.0: 2021.0]
 
-# make plot line smoother (need to uncomment scipy.interpolate)
-    # line_plot = make_interp_spline('INSTALL_DATE', 'NUM')
-    # num_plot = line_plot(install_plot)
-    # plt.plot(install_plot, num_plot)
+    sns.lineplot(data=year, x='INSTALL_DATE', y='NUM', hue='CATEGORY')
 
     plt.xlabel('Dates of Installment')
     plt.ylabel('Num of Bike Infrastructures')
-    plt.title('Number of Bike Infrastructures 2010-2020')
-    # plt.show()
+    plt.title('Number of Bike Infrastructures built in 2010-2021')
     plt.savefig('bike_infrastructures_plot.png')
 
     # map the bike infractures
 
-    planned_file = gpd.read_file('Data/Planned_Bike_Facilities/Planned_Bike_Facilities.shp')
-    print(planned_file.columns)
-    # planned_file.plot()
+    planned_file = gpd.read_file(
+        'Data/existing_bike_facilities/Existing_Bike_Facilities.shp')
 
     fig, [[ax1, ax2], [ax3, ax4]] = plt.subplots(2, 2, figsize=(20, 10))
-    map_2010 = planned_file.loc[2010.0]
-    map_2010.plot(column='Category', legend=True, ax=ax1)
-    ax1.set_title('2010 infrastructures')
 
-    map_2012 = planned_file.loc[2012.0]
-    map_2012.plot(column='Category', legend=True, ax=ax2)
-    ax2.set_title('2012 infrastructures')
+    # plot 2010
+    data_2010 = planned_file[planned_file['INSTALL_DA'] == '2010-12-31']
+    planned_file.plot(color='#EEEEEE', ax=ax1)
+    data_2010.plot(column='CATEGORY', legend=True, ax=ax1)
+    ax1.set_title('Bike Infrastructures built in 2010')
 
-    map_2014 = planned_file.loc[2014.0]
-    map_2014.plot(column='Category', legend=True, ax=ax3)
-    ax3.set_title('2014 infrastructures')
+    # plot 2014
+    data_2014 = planned_file[planned_file['INSTALL_DA'] == '2014-12-31']
+    data_2014 = planned_file[planned_file['INSTALL_DA'] == '2013-12-31']
+    data_2014 = planned_file[planned_file['INSTALL_DA'] == '2012-12-31']
+    planned_file.plot(color='#EEEEEE', ax=ax2)
+    data_2014.plot(column='CATEGORY', legend=True, ax=ax2)
+    ax2.set_title('Bike Infrastructures built in 2012-2014')
 
-    map_2020 = planned_file.loc[2020.0]
-    map_2020.plot(column='Category', legend=True, ax=ax4)
-    ax4.set_title('2020 infrastructures')
-    plt.savefig('bike_infrastructures_map.png')
+    # plot 2018
+    data_2018 = planned_file[planned_file['INSTALL_DA'] == '2018-12-31']
+    data_2018 = planned_file[planned_file['INSTALL_DA'] == '2017-12-31']
+    data_2018 = planned_file[planned_file['INSTALL_DA'] == '2016-12-31']
+    data_2018 = planned_file[planned_file['INSTALL_DA'] == '2015-12-31']
+    planned_file.plot(color='#EEEEEE', ax=ax3)
+    data_2018.plot(column='CATEGORY', legend=True, ax=ax3)
+    ax3.set_title('Bike Infrastructures built in 2015-2018')
+
+    # plot 2021
+    data_2021 = planned_file[planned_file['INSTALL_DA'] == '2021-12-31']
+    data_2021 = planned_file[planned_file['INSTALL_DA'] == '2020-12-31']
+    data_2021 = planned_file[planned_file['INSTALL_DA'] == '2019-12-31']
+    data_2021 = planned_file[planned_file['INSTALL_DA'] == '2018-12-31']
+    planned_file.plot(color='#EEEEEE', ax=ax4)
+    data_2021.plot(column='CATEGORY', legend=True, ax=ax4)
+    ax4.set_title('Bike Infrastructures built in 2018-2021')
+
+    # plt.savefig('bike_infrastructures_map.png')
 
 
 if __name__ == "__main__":
