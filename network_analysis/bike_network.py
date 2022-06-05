8531027e39fa9eb5958b9bf49a59fa2313081d06
sbiaggne <sbiaggne@uw.edu>
2022-06-04 18:29:29 -0700

    network analysis

--- network_analysis/bike_network.py
+++ network_analysis/bike_network.py
@@ -1,16 +1,13 @@
 import networkx as nx
+import pandas as pd
 import pickle
 
 
-def make_graph(filename):
+def make_graph(data):
     """
     Creates networkx graph using bike infrastructure data from pandas
-    DataFrame stored in pickle file 'filename'.
+    DataFrame 'data'.
     """
-    # Load bike infrastructure data:
-    with open(filename) as f:
-        data = pickle.load(f)
-
     # Put data in networkx graph:
     graph = nx.from_pandas_edgelist(
         data, 'START', 'END', edge_attr=True
@@ -46,17 +43,56 @@ def get_total_connectivity(graph):
     return total
 
 
-def get_all_networks(filename):
+def get_all_networks():
     """
-    Creates networkx graph from 'filename' and finds connectivity
-    metric for each kind of bike infrastructure for every year,
+    Creates networkx graphs from bike infrastructure data and finds
+    connectivity metric for each kind of bike infrastructure for every year,
     along with an overall network connectivity. This means six networks
-    per year
+    per year, one for Bike Lanes (BL), Protected Bike Lanes (PBL),
+    Neighborhood Greenways (NGW), Sharrows (SHW), Off-Street Paths
+    (OFFST), and the overall bike network.
+
+    Dates start with 2012, since that is the earliest year with
+    available bike ridership data from any of the bike counters.
     """
+    # Making pandas DataFrame to track connectivity of each kind and overall
+    # network connectivity by year:
+    connectivity = pd.DataFrame(index=[y for y in range(2012, 2022)])
+
+    existing_inf = pd.read_csv(
+        'Data\\refined_data\\existing_facilities_data_csv.csv'
+    )
+
+    # Going by each year that has available ridership data:
+    for year in range(2012, 2022):
+        # Defining past years to be included for each yearly iteration:
+        present_data = existing_inf[existing_inf['INSTALL_DATE'] <= year]
+
+        # Analyzing overall present network for this year:
+        connectivity.loc[year, 'Overall'] = get_total_connectivity(
+            make_graph(present_data)
+        )
+
+        # Analyzing each infrastructure type for this year:
+        for inf_type in ['BKF-BL', 'BKF-PBL', 'BKF-NGW', 'BKF-SHW',
+                         'BKF-OFFST']:
+            # Applying a mask to only get desired infrastructure
+            # type for the year:
+            inf_data = present_data[present_data['CATEGORY'] == inf_type]
+            # Making, analyzing specific infrastructure network:
+            connectivity.loc[year, inf_type] = get_total_connectivity(
+                make_graph(inf_data)
+            )
+
+    # Saving connectivity DataFrame as pickle file:
+    with open('network_analysis\\connectivity.pickle', 'wb') as f2:
+        pickle.dump(connectivity, f2)
+    # Saving connectivity DataFrame as CSV:
+    connectivity.to_csv('network_analysis\\connectivity_csv.csv')
 
 
 def main():
-    pass
+    get_all_networks()
 
 
 if __name__ == '__main__':
--- network_analysis/testing.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-with open('Data\\refined_data\\existing_facilities_data_csv.csv') as f:
-    df = pd.read_csv(f)
-
-
-print(df.loc[0, 'INSTALL_DATE'] >= 2011)
